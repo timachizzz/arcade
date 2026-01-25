98d6c106cad16f76f86aee396ffd9aaa133eba93
Ral_Say_Wiz_Tea <litepubg69@mail.ru>
2026-01-25 18:20:12 +0700

    Создал Pinewheel (хаотичное движение с вращением по кривой(заданная прямая с отражением угла движения) # стоит проверить спавн квадратов, чтобы они друг в друге не появлялись

--- enemies.py
+++ enemies.py
@@ -1,6 +1,6 @@
 from random import choice, randint
 import arcade
-
+from math import cos, sin, radians
 
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
@@ -29,8 +29,39 @@ class Square(Enemy):
             self.act = '-='
         self.i += 1
 
-# class Pinwheel(Enemy):
-    # ...
+class Pinwheel(Enemy):
+    def __init__(self):
+        super().__init__('pic/Pinwheel.png')
+        self.image = arcade.load_image('pic/Pinwheel.png')
+        self.scale = 1.0
+        self.speed = 150
+        self.move_angle = randint(0, 360)
+        self.rotation_angle = 0
+        self.rotation_speed = 200
+        self.original_image = self.image.copy()
+        self.center_x = randint(80, SCREEN_WIDTH - 60)
+        self.center_y = randint(80, SCREEN_HEIGHT - 60)
+
+    def move(self, delta_time):
+        self.rotation_angle += self.rotation_speed * delta_time
+        self.angle = self.rotation_angle % 360
+        self.rotation_angle += self.rotation_speed * delta_time
+        self.center_x += self.speed * cos(radians(self.move_angle)) * delta_time
+        self.center_y += self.speed * sin(radians(self.move_angle)) * delta_time
+
+        if self.center_x < 50:
+            self.move_angle = 180 - self.move_angle
+
+        elif self.center_x > SCREEN_WIDTH - 50:
+            self.move_angle = 180 - self.move_angle
+
+        if self.center_y < 50:
+            self.move_angle = -self.move_angle
+
+        elif self.center_y > SCREEN_HEIGHT - 50:
+            self.move_angle = -self.move_angle
+
+        self.move_angle %= 360
 
 class Rocket(Enemy):
     def __init__(self):
