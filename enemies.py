b5775a0b53420d5807614dbdfebe7f190a229ab6
Ral_Say_Wiz_Tea <litepubg69@mail.ru>
2026-01-25 17:09:03 +0700

    Создал врага (Rocket(Enemy)), имеет поведение: спавн в случайном месте на экране и постоянное движение с разворотом, при столкновении со стеной. В дальнейшем добавлю плавный поворот (lerp)

--- enemies.py
+++ enemies.py
@@ -1,5 +1,4 @@
-from random import choice
-
+from random import choice, randint
 import arcade
 
 
@@ -33,10 +32,51 @@ class Square(Enemy):
 # class Pinwheel(Enemy):
     # ...
 
+class Rocket(Enemy):
+    def __init__(self):
+        super().__init__('pic/rocket.png')
+        self.image = arcade.load_image('pic/rocket.png')
+        self.scale = 1.2
+        self.speed = 300
+        self.angle = 0
+        self.original_image = self.image.copy()
+        self.center_x = randint(50, SCREEN_WIDTH - 50)
+        self.center_y = randint(50, SCREEN_HEIGHT - 50)
+        self.direction = choice([
+            [1, 0],
+            [-1, 0],
+            [0, 1],
+            [0, -1]
+        ])
+        self.update_rotation()
 
-# class Rocket(Enemy):
-    # ...
+    def update_rotation(self):
+        """Обновляет угол поворота в зависимости от направления"""
+        if self.direction == [1, 0]:  # вправо
+            self.angle = 90
+        elif self.direction == [-1, 0]:  # влево
+            self.angle = -90
+        elif self.direction == [0, 1]:  # вниз
+            self.angle = 0
+        elif self.direction == [0, -1]:  # вверх
+            self.angle = 180
 
+    def move(self, delta_time):
+        self.center_x += self.direction[0] * self.speed * delta_time
+        self.center_y += self.direction[1] * self.speed * delta_time
+
+        if self.left <= 0: #левый край
+            self.direction[0] = 1 # вправо
+            self.update_rotation()
+        elif self.right >= SCREEN_WIDTH: # правый край
+            self.direction[0] = -1 # влево
+            self.update_rotation()
+        if self.top <= 100: # нижний край
+            self.direction[1] = 1 #вверх
+            self.update_rotation()
+        elif self.bottom >= SCREEN_HEIGHT - 80: # верхний край
+            self.direction[1] = -1 # вниз
+            self.update_rotation()
 
 def get_enemies():
     """Возвращает список всех противников (для появления на игровом поле)"""
